78c417a9d8e9c84faa120cb852be48e619c7bc9c
Ashlee Tolentino <104875387+ashlee-tolentino@users.noreply.github.com>
2023-01-25 14:53:30 -0500

    added class header/prototypes

---
+++ MyVector.h
@@ -0,0 +1,22 @@
+#pragma once
+
+template <class T>
+class MyVector{
+private:
+  int max_size;
+  int current_size;
+  T *arr;
+
+public:
+  MyVector();
+  int size();
+  int capacity();
+  bool empty();
+  void push_back(T item);
+  void pop_back(int n);
+  void pop_back();
+  void clear();
+  T &operator[] (int i);
+};
+
+#include "MyVector.cxx"
